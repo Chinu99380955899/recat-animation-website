b1057e5442415e409034f818b6933bb4c10e745c
chinu99380955899 <sutarchinmaya385@gmail.com>
2024-07-16 14:51:29 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>CHINMAYA</h1>
+        <p>sutarchinmaya385@gmail,com</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a
+            href="https://www.youtube.com/channel/UCrFwuzLNBta1ArFdnKAOsIQ"
+            target={"blank"}
+          >
+            Youtube
+          </a>
+          <a href="https://www.instagram.com/chinmaya_sutar_/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/Chinu99380955899" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
